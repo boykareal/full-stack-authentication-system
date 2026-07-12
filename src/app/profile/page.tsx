4023e474c65dbaffa9d99ad11cb5627a3d3f22ad
boykareal <vaibhavx3sharma@gmail.com>
2026-07-12 06:23:41 +0530

    add a bug fix for logout button

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -45,12 +45,6 @@ export default function ProfilePage(){
           Logout
         </button>
         <button
-          onClick={logout}
-          className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Logout
-        </button>
-        <button
           onClick={getUserDetails}
           className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
